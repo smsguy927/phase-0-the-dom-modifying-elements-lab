ed26fcfc956e7b88fda5512153293707f4279b14
smsguy927 <smsguy927@gmail.com>
2021-03-25 01:10:56 -0500

    Done!

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+let body = document.getElementsByTagName('body')[0];
+body.removeChild(body.querySelector('#main'));
+let newHeader = document.createElement('h1');
+newHeader.innerHTML = 'YOUR-NAME is the champion';
+newHeader.id = 'victory';
+body.appendChild(newHeader);
+
